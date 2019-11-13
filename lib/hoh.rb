c663bb10994e8e188867beebcc2fb36c1775dd69
hylobates-lar <alison.v.quaglia@gmail.com>
2019-11-13 17:06:35 -0500

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -3,43 +3,48 @@
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
+}
+
+PORTION_2 = {
+  label: "Order"
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+  }
+}
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 
 def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+  new_hoh = PORTION_1
+  PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+  PORTION_2[:sub_category] = PORTION_3
+  PORTION_3[:sub_category][:sub_category] = PORTION_4
+  new_hoh
 end
+
+# Remember:
+#  Kingdom
+#  Phylum
+#  Class
+#  Order
+#  Family
+#  Genus
+#  Species
+# So, if we have the "Kingdom" node we should be able to "tunnel" into the
+# HoH all the way to Species!
